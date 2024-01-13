2bab31a46b3ed7516709985a973844d1697f261d
Josesx506 <josesomojola@gmail.com>
2024-01-13 13:45:40 -0700

    project 1 corrections

--- 01_Intro/04_Project/Task2.py
+++ 01_Intro/04_Project/Task2.py
@@ -11,20 +11,37 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
     
-    top_number,duration = None,None
+    # Old implementation that only get the 
+    # top_number,duration = None,None
+    # for rows in calls:
+    #     call_dur = int(rows[3])
+    #     if duration == None or call_dur>=duration:
+    #         duration = call_dur
+    #         top_number = rows[1]
+    
+    phone_dict={}
+    for call in calls:
+        # Extract the minutes of the calls placed
+        phone_dict[call[0]] = phone_dict.get(call[0], 0) + int(call[3])
 
-    for rows in calls:
-        call_dur = int(rows[3])
-        if duration == None or call_dur>=duration:
-            duration = call_dur
-            top_number = rows[1]
+        # Extract the minutes of the calls received
+        phone_dict[call[1]] = phone_dict.get(call[1], 0) + int(call[3])
+    
+    duration, top_number = max(zip(phone_dict.values(), phone_dict.keys()))#[0,1]
 
 print(f"{top_number} spent the longest time, {duration} seconds, on the phone during September 2016.")
+
+# import json
+# print(json.dumps(phone_dict, indent=4, sort_keys=True, default=str))
 
 """
 TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
 also time spent on the phone.
+
+We have to find the phone number which spent the longest time on the phone (calling and receiving) and 
+also the total time spent instead you have only considered the time spent.
+
 Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
--- 01_Intro/04_Project/Task4.py
+++ 01_Intro/04_Project/Task4.py
@@ -4,33 +4,30 @@ It's ok if you don't understand how to read files.
 """
 import csv
 
+potential_telemarketers = set() # Set to hold unique numbers that place phone calls only
+non_telemarketers = set()       # Set to hold every other number that does more than placing calls
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-    all_texters = []
-
-    for row in texts:
-        all_texters.append(row[0])
+    # Telemarketers cannot send and recieve texts so all the numbers texting are non-telemarketers
+    for text in texts:
+        non_telemarketers.add(text[0]) # Text senders
+        non_telemarketers.add(text[1]) # Text receivers
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-    all_callers = []
+    # Telemarketers can only place calls
+    for call in calls:
+        potential_telemarketers.add(call[0]) # Outgoing calls
+        non_telemarketers.add(call[1])       # Incoming calls
 
-    for row in calls:
-        all_callers.append(row[0])
-
-unique_texters = list(set(all_texters))
-unique_callers = list(set(all_callers))
-
-telemarketers = []
-
-for idx,num in enumerate(unique_callers):
-    if num not in unique_texters:
-        telemarketers.append(num)
-
+# Subtract all non_tele numbers from outgoing
+telemarketers = potential_telemarketers.difference(non_telemarketers)
+# Sort the results and print line by line
 telemarketers = "\n".join(sorted(telemarketers))
 
 print(f"These numbers could be telemarketers: \n{telemarketers}")
@@ -41,6 +38,9 @@ The telephone company want to identify numbers that might be doing
 telephone marketing. Create a set of possible telemarketers:
 these are numbers that make outgoing calls but never send texts,
 receive texts or receive incoming calls.
+
+Possible telemarketers numbers are those which can only make outgoing calls but can't receive 
+any incoming calls, send texts, and receive texts.
 
 Print a message:
 "These numbers could be telemarketers: "
